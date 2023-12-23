a6f1dd574f2fa88501135cc54a0c01e8fc205fd4
shwetalavhe23 <shweta.lavhe.23@gmail.com>
2023-12-24 01:13:30 +0530

    Comment added in App

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
 const heading = React.createElement("h1", {}, "Hello World!");
+console.log(
+  "ReactJs create element creates an JS object of an element! And not an HTML element directly!"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
